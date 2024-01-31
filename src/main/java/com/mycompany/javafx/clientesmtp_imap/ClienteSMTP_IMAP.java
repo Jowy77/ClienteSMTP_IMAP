8477e367440f39f4a21bfee7b80d8efa3f34a008
Jowy77 <Iproyectz@hotmail.com>
2024-01-31 18:42:51 +0000

    first commit

---
+++ src/main/java/com/mycompany/javafx/clientesmtp_imap/ClienteSMTP_IMAP.java
@@ -0,0 +1,8 @@
+package com.mycompany.javafx.clientesmtp_imap;
+
+public class ClienteSMTP_IMAP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
